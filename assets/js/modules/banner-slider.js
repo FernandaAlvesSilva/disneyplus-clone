4ea901550c11734134a2bc0abd69171d7d3ca51e
FernandaAlvesSilva <feralves2003@gmail.com>
2025-04-07 14:24:49 -0300

    atualizar depois

---
+++ assets/js/modules/banner-slider.js
@@ -0,0 +1,56 @@
+const sliderItems = document.querySelectorAll('[data-banner="item"]');
+const slider = document.querySelector('[data-banner="slider"]');
+const state = {
+    mouseDownPosition: 0
+}
+
+function preventDefault(event){
+    event.preventDefault();
+}
+
+
+function onMouseDown(event){
+    const slide = event.currentTarget
+    state.mouseDownPosition = event.clientX
+    console.log('mousedown -', event.clientX)
+    slide.addEventListener('mousemove', onMouseMove)
+    
+}
+
+
+function onMouseUp(event){
+    const slide = event.currentTarget
+    console.log(event.clientX)
+    slide.removeEventListener('mousemove', onMouseMove)
+   
+}
+
+function onMouseMove(event){
+    slider.style.transforme = 'translateX(s{event.clientX - state.mouseDownPosition}px)'
+    console.log('mousemove -', event.clientX)
+}
+
+function onMouseLeave(event){
+    const slide = event.currentTarget
+    slide.removeEventListener('mousemove', onMouseMove)
+}
+
+function setListeners(){
+    sliderItems.forEach(function(slide, index){
+        const link =  slide.querySelector('.banner-slider__link')
+          link.addEventListener('click',preventDefault)
+          slide.addEventListener('dragstart',preventDefault)
+          slide.addEventListener('mousedown', onMouseDown)
+          slide.addEventListener('mouseup', onMouseUp)
+          slide.addEventListener('mousemove', onMouseMove)
+          slide.addEventListener('mouseleave', onMouseLeave)
+      })
+}
+
+function init(){
+    setListeners()
+}
+
+export default {
+    init
+}
